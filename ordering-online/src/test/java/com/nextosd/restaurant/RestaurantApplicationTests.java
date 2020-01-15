852e74b7e148e1772be85a48d2a8c73ef033d8a4
chenglin <LvLin22@126.com>
2020-01-15 09:44:26 +0800

    重命名

--- ordering-online/src/test/java/com/nextosd/restaurant/RestaurantApplicationTests.java
+++ ordering-online/src/test/java/com/nextosd/restaurant/RestaurantApplicationTests.java
@@ -1,10 +1,10 @@
-package com.nextosd.orderingonline;
+package com.nextosd.restaurant;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OrderingOnlineApplicationTests {
+class RestaurantApplicationTests {
 
 	@Test
 	void contextLoads() {
