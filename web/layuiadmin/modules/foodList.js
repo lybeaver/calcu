c3656e9a8588c32e846203ab831cfc831f7696c6
chenglin <LvLin22@126.com>
2020-01-21 13:36:07 +0800

    完善菜品实例

--- web/layuiadmin/modules/foodList.js
+++ web/layuiadmin/modules/foodList.js
@@ -1,35 +1,47 @@
 layui.define(function (exports) {
-
-
     layui.use(['setter', 'table'], function () {
         var setter = layui.setter,
             table = layui.table;
-
         //第一个实例
         table.render({
-            elem: '#foodTable'
+            id: 'foodReload'
+            , elem: '#foodTable'
             , height: 500
-            , url: setter.address + 'selectAll' //数据接口
+            , url: setter.address + 'menu/page' //数据接口
             , page: true //开启分页
             , cols: [[ //表头
                 { field: 'foodName', title: '名称', width: 230, sort: true }
                 , { field: 'foodType', title: '类型', width: 230, sort: true }
                 , { field: 'foodPrice', title: '价格', width: 150, sort: true }
                 , { field: 'foodNum', title: '剩余数量', width: 120, sort: true }
+                , { fixed: 'right', title: '操作', width: 180, align: 'center', toolbar: '#toolBar' }
                 , { field: 'foodId', title: 'ID', hide: true }
             ]], done: function (res, curr, count) {
-
                 console.log(res);
-
-                //得到当前页码
-                console.log(curr);
-
-                //得到数据总量
-                console.log(count);
             }
         });
-
+        //监听工具条
+        table.on('tool(demo)', function (obj) {
+            var data = obj.data;
+            if (obj.event === 'del') {
+                layer.confirm('真的删除行么', function (index) {
+                    var json = eval('(' + JSON.stringify(data) + ')');//String转json
+                    log(json['storeId'])
+                    $.post("路径", { "参数": null }, function (msg) {
+                        if (msg.code == 0) {
+                            obj.del();
+                            layer.close(index);
+                        } else {
+                            layer.msg('删除失败！');
+                        }
+                    })
+                });
+                //编辑
+            } else if (obj.event === 'edit') {
+                var json = eval('(' + JSON.stringify(data) + ')');//String转json
+                layerOpen('路径' + '参数', '编辑店铺信息', '670px;', '530px;');
+            }
+        });
     });
-
     exports('foodList', {})
 });
