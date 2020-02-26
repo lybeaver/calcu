850274832bee2bb04bfc55789d07225996f31da2
Shim333 <59866357+Shim333@users.noreply.github.com>
2020-02-26 15:39:15 +0800

    注册页面

--- ordering-online/src/main/java/com/nextosd/restaurant/controller/UserController.java
+++ ordering-online/src/main/java/com/nextosd/restaurant/controller/UserController.java
@@ -2,6 +2,7 @@ package com.nextosd.restaurant.controller;
 
 import java.awt.image.BufferedImage;
 import java.io.OutputStream;
+import java.util.Date;
 import java.util.Map;
 
 import javax.imageio.ImageIO;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.nextosd.restaurant.beans.User;
 import com.nextosd.restaurant.beans.other.LoginBean;
 import com.nextosd.restaurant.mapper.UserMapperBack;
+import com.nextosd.restaurant.mapper.common.UserMapper;
 import com.nextosd.restaurant.utils.Md5Util;
 import com.nextosd.restaurant.utils.VerifyUtil;
 
@@ -26,31 +28,86 @@ import com.nextosd.restaurant.utils.VerifyUtil;
 @RequestMapping("/user")
 public class UserController {
 	
-	//@Autowired
-	//private UserMapper userMapper;
+	@Autowired
+	private UserMapper userMapper;
 	
 	@Autowired
 	private UserMapperBack userMapperBack;
 	
 	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 	
-	
-	
 	//获取验证码
-	@GetMapping("/verify-code")
-	public void getCode(HttpServletResponse response,HttpSession session) throws Exception{
-	    Map<String, Object> map = VerifyUtil.createImage();
-	    String  sb = (String) map.get("code");
-	    logger.info("获取验证码："+sb);
-	    session.setAttribute("checkCode", sb);
-	    logger.info("session放入验证码"+(String)session.getAttribute("checkCode"));
-	    //将图片输出给浏览器
-	    BufferedImage image = (BufferedImage) map.get("image");
-	    response.setContentType("image/png");
-	    OutputStream os = response.getOutputStream();
-	    ImageIO.write(image, "png", os);
+		@GetMapping("/verify-code")
+		public void getCode(HttpServletResponse response,HttpSession session) throws Exception{
+		    Map<String, Object> map = VerifyUtil.createImage();
+		    String  sb = (String) map.get("code");
+		    logger.info("获取验证码："+sb);
+		    session.setAttribute("checkCode", sb);
+		    logger.info("session放入验证码"+(String)session.getAttribute("checkCode"));
+		    //将图片输出给浏览器
+		    BufferedImage image = (BufferedImage) map.get("image");
+		    response.setContentType("image/png");
+		    OutputStream os = response.getOutputStream();
+		    ImageIO.write(image, "png", os);
+		}
+
+	/**
+	  * 	登录
+	  * @Title: UserController.java  
+	  * @param 
+	  * @return  
+	  * @date 2020年2月24日
+	 */
+	@PostMapping(value = "/login")
+	public int login(LoginBean lgBean,HttpSession session) throws Exception {
+		int result = 0;
+		String check = (String)session.getAttribute("checkCode");
+		logger.info("session:"+check);
+		logger.info("用户名:"+lgBean.getUserName());
+		if (!lgBean.equals(check)) {
+			logger.info("验证码错误");
+			result = 2 ;
+			return result;
+		}
+		User exUser = userMapperBack.selectUserByUserName(lgBean.getUserName());
+		if (exUser == null) {
+			logger.info("用户名不存在");
+			return result;
+		}
+		String md5EmcodeString = Md5Util.md5Encode(lgBean.getPassword());
+		if (exUser.getPassword().equals(md5EmcodeString)) {
+			logger.info("登录成功");
+			result = 1;
+			return result;
+		}
+		logger.info("密码错误");
+		return result;
 	}
-
 	
-
+	@PostMapping(value = "/insertUser")
+	public int insertUser(User user) throws Exception {
+		System.out.println("开始注册");
+		String pd = user.getPassword();
+		logger.info(user.getUserName());
+		String md5EncodeString = Md5Util.md5Encode(pd);
+		logger.info("md5加密后"+md5EncodeString);
+		user.setPassword(md5EncodeString);
+		Date now = new Date();
+		user.setLogTime(now);
+		user.setUserType(2);//2的等级为用户，0为管理者，1为商家
+		int result = userMapper.insertSelective(user);
+		return result;
+	}
+	
+	@GetMapping(value = "/checkUser")
+	public int checkMessage(String userName) {
+		User user = userMapperBack.selectUserByUserName(userName);
+		int result = 0;
+		if (user == null) {
+			//如果用户可用，返回1
+			result = 1;
+		}
+		//如果用户存在，返回0
+		return result;
+	}
 }
