e04067b491db90f49c44a739923b7981bd5a9815
孙书彬 <59867453+shubin-sun@users.noreply.github.com>
2020-02-26 16:42:20 +0800

    重新设计导航栏

--- web/layuiadmin/modules/foodList.js
+++ web/layuiadmin/modules/foodList.js
@@ -7,20 +7,10 @@ layui.define(function (exports) {
         table.render({
             id: 'foodReload'
             , elem: '#foodTable'
-            , height: 500
+            , height: 472
             , url: setter.address + 'menu/page' //数据接口
             , page: true //开启分页
-            , limits: [10,20,30]
-            , jump: function(obj, first){
-                //obj包含了当前分页的所有参数，比如：
-                console.log(obj.curr); //得到当前页，以便向服务端请求对应页的数据。
-                console.log(obj.limit); //得到每页显示的条数
-                
-                //首次不执行
-                if(!first){
-                    //do something
-                }
-            }
+            , limits: [10,20,50]
             , cols: [[ //表头
                 { field: 'foodName', title: '名称', width: 200, sort: true }
                 , { field: 'foodType', title: '类型', width: 150, sort: true }
